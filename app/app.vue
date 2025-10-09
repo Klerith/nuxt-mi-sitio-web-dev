b03e8be2c3419d29e6c9bd785986a76ed9049e2b
Fernando Herrera <fernando.herrera85@gmail.com>
2025-10-09 10:08:38 -0400

    Add initial app structure with main components and styles
    - Implemented app.vue with a reactive count and button functionality.
    - Added main.css for styling the application.
    - Created sleep utility function for asynchronous operations.

--- app/app.vue
+++ app/app.vue
@@ -1,11 +1,31 @@
+<script lang="ts" setup>
+const count = ref(10);
+await sleep(2000);
+
+count.value = 20;
+</script>
+
 <template>
   <div>
-    <h1>Hola Mundo</h1>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt hic
-      delectus impedit doloremque magni perferendis quod illum animi sed atque
-      assumenda, numquam voluptatum aspernatur necessitatibus totam corrupti
-      obcaecati. Sunt, facere!
-    </p>
+    <h1>Hola Mundo - {{ count }}</h1>
+
+    <button @click="count++">Click me +</button>
   </div>
 </template>
+
+<style>
+/* body {
+  background-color: #020420;
+  font-family: 'Roboto', sans-serif;
+  color: #fff;
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  color: #00dc82;
+} */
+</style>
