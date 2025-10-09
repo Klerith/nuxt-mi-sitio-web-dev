f38993d4d7cd5f06b68bced9dcf36cff02082978
Fernando Herrera <fernando.herrera85@gmail.com>
2025-10-09 10:33:05 -0400

    Refactor app structure by removing unused components and adding basic page templates

--- app/app.vue
+++ app/app.vue
@@ -1,15 +1,6 @@
-<script lang="ts" setup>
-const count = ref(10);
-await sleep(2000);
-
-count.value = 20;
-</script>
-
 <template>
   <div>
-    <h1>Hola Mundo - {{ count }}</h1>
-
-    <button @click="count++">Click me +</button>
+    <NuxtPage />
   </div>
 </template>
 
---
+++ app/pages/products.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>Page: products</div>
+</template>
+
+<style scoped></style>
