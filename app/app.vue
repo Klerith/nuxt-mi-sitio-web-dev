cd59854d2b565afdab4b0a0c23a55126156a9603
Fernando Herrera <fernando.herrera85@gmail.com>
2025-10-09 09:45:34 -0400

    first commit

---
+++ app/app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Hola Mundo</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt hic
+      delectus impedit doloremque magni perferendis quod illum animi sed atque
+      assumenda, numquam voluptatum aspernatur necessitatibus totam corrupti
+      obcaecati. Sunt, facere!
+    </p>
+  </div>
+</template>
