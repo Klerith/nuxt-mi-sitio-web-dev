d5f24f0b72127c1a1167aecf0ae4e980ee2418c3
Fernando Herrera <fernando.herrera85@gmail.com>
2025-10-15 12:33:05 -0400

    Add @nuxt/ui module and update configuration
    - Included '@nuxt/ui' in nuxt.config.ts modules for enhanced UI components.
    - Updated package.json to include '@nuxt/ui' and 'tailwindcss' dependencies.
    - Adjusted pnpm-lock.yaml to reflect new dependencies.
    - Modified .vscode/settings.json for Tailwind CSS integration.
    - Refactored app structure by wrapping NuxtLayout in UApp component in app.vue.
    - Simplified error.vue by utilizing UError component for error handling.
    - Updated main.css to import Tailwind CSS and @nuxt/ui styles.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,7 +17,13 @@ export default defineNuxtConfig({
     },
   },
 
-  modules: ['@nuxt/eslint', '@nuxt/fonts', '@nuxt/icon', '@nuxt/image'],
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/icon',
+    '@nuxt/image',
+    '@nuxt/ui',
+  ],
 
   // SPA - Single Page Application
   // ssr: false,
