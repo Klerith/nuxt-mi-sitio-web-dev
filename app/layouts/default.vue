dc222bf9fd5791e23a580ef476038192de68e18f
Fernando Herrera <fernando.herrera85@gmail.com>
2025-10-24 13:32:28 -0400

    Refactor layout structure: enhance default layout with SharedHeader and improved main content organization

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,6 +1,13 @@
 <template>
-  <div>
-    <MainNavigation />
-    <slot />
+  <div class="flex flex-col h-screen">
+    <!-- <MainNavigation /> -->
+    <!-- Header -->
+    <SharedHeader />
+
+    <main class="container mx-auto flex-1 mt-2">
+      <slot />
+    </main>
+
+    <!-- Footer -->
   </div>
 </template>
