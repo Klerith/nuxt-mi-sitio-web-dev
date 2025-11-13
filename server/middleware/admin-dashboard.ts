372e66d841d5c520b3da4229cfcf8e5fe314b302
Fernando Herrera <fernando.herrera85@gmail.com>
2025-11-13 13:30:25 -0500

    Refactor admin dashboard middleware by removing commented code and standardizing error message format

--- server/middleware/admin-dashboard.ts
+++ server/middleware/admin-dashboard.ts
@@ -1,20 +1,15 @@
 export default defineEventHandler(async (event) => {
-  // console.log('New request: ' + getRequestURL(event));
-
   if (!event.path.startsWith('/api/admin')) {
     return;
   }
 
   const session = await requireUserSession(event);
-  // const session = await getUserSession(event);
   const hasAdminRole = session.user.roles.includes('admin');
 
   if (!hasAdminRole) {
     throw createError({
       statusCode: 401,
-      message: `Unauthorized`,
+      message: 'Unauthorized',
     });
   }
-
-  return;
 });
