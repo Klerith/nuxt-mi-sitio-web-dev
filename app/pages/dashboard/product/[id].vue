207dfb2df5d91c38d54d061d91f5aaa19dde7196
Fernando Herrera <fernando.herrera85@gmail.com>
2025-11-13 12:38:18 -0500

    Add product creation link and implement product detail page

---
+++ app/pages/dashboard/product/[id].vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+const route = useRoute();
+
+// Variables
+const rawId = route.params.id as string;
+</script>
+
+<template>
+  <h1>Admin Product Page</h1>
+  <h3>{{ rawId }}</h3>
+</template>
